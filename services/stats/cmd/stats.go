6064c3ed3076f179040e3b86db6788132bcadce1
mikuspikus <pyro74rus@gmial.com>
2020-07-18 15:49:11 +0500

    Fix wrong pkg usage

--- services/stats/cmd/stats.go
+++ services/stats/cmd/stats.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/caarlos0/env"
 	"github.com/mikuspikus/news-aggregator-go/pkg/tracer"
-	"github.com/mikuspikus/news-aggregator-go/services/news/pkg/news"
 	"github.com/mikuspikus/news-aggregator-go/services/stats/pkg/stats"
 	"log"
 )
@@ -14,7 +13,7 @@ func main() {
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("could not parse env vars for config: %v", err)
 	}
-	tracer, closer, err := tracer.NewTracer("news", cfg.JaegerAddress)
+	tracer, closer, err := tracer.NewTracer("stats", cfg.JaegerAddress)
 	defer closer.Close()
 	if err != nil {
 		log.Fatal(err)
@@ -22,7 +21,7 @@ func main() {
 
 	connString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbPort, cfg.DbDatabase)
 	apps := map[string]string{cfg.AppID: cfg.AppSecret}
-	service, err := news.New(connString, cfg.RedisHost, cfg.RedisPwd, cfg.RedisDb, apps)
+	service, err := stats.New(connString, cfg.RedisHost, cfg.RedisPwd, cfg.RedisDb, apps)
 	if err != nil {
 		log.Fatal(err)
 	}
