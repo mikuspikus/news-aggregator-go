da0b8ff594f23b1a7a0f77afe3ff856f9925156b
mikuspikus <pyro74rus@gmial.com>
2020-08-05 20:16:03 +0500

    Updated Table for stats

--- services/frontend/src/components/stats/TableFull.vue
+++ services/frontend/src/components/stats/TableFull.vue
@@ -1,64 +1,92 @@
 <template>
   <div id="stats-full">
-    <b-table
-      :items="items"
-      :fields="fields"
-      stacked="md"
-      :current-page="currentPage"
-      striped
-      small
-      bordered
-      dark
-    >
-      <template v-slot:cell(io)="row">
-        <b-row>
-          <b-col>
-            <b-row>
-              <b-col>Input:</b-col>
-              <b-col>{{ _size(row.item.input) }}</b-col>
-            </b-row>
-
-            <b-row>
-              <b-col>Output:</b-col>
-              <b-col>{{ _size(row.item.output) }}</b-col>
-            </b-row>
-          </b-col>
-          <b-col align-self="center" md="auto" class="m-2">
-            <b-button
-              v-if=" _size(row.item.input) || _size(row.item.output)"
+    <b-container fluid>
+      <b-row>
+        <b-col>
+          <b-form-group
+            label="Per page"
+            label-cols-sm="6"
+            label-cols-md="4"
+            label-cols-lg="3"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="perPageSelect"
+            class="mb-0"
+          >
+            <b-form-select
+              v-model="pageSize"
+              id="perPageSelect"
               size="sm"
-              variant="dark"
-              @click="row.toggleDetails"
-            >
-              <b-icon-arrow-bar-up v-if="row.detailsShowing" />
-              <b-icon-arrow-bar-down v-else />
-            </b-button>
-          </b-col>
-        </b-row>
-      </template>
-
-      <template v-slot:row-details="row">
-        <b-card class="text-left" bg-variant="dark">
-          <b-row v-if="_size(row.item.input)">
-            <json-view
-              :maxDepth="1"
-              :rootKey="'Input'"
-              :data="row.item.input"
-              :colorScheme="'dark'"
+              :options="pageOptions"
+              @input="changePageSize"
             />
-          </b-row>
+          </b-form-group>
+        </b-col>
 
-          <b-row v-if="_size(row.item.output)">
-            <json-view
-              :maxDepth="1"
-              :rootKey="'Output'"
-              :data="row.item.output"
-              :colorScheme="'dark'"
-            />
+        <b-col sm="7" md="6">
+          <b-pagination
+            v-model="currentPage"
+            @input="changePage"
+            :total-rows="pageCount"
+            :per-page="1"
+            first-number
+            last-number
+            align="fill"
+            size="sm"
+          />
+        </b-col>
+      </b-row>
+      <b-table :items="items" :fields="fields" stacked="md" striped small bordered dark>
+        <template v-slot:cell(io)="row">
+          <b-row>
+            <b-col>
+              <b-row>
+                <b-col>Input:</b-col>
+                <b-col>{{ row.item.input.size }}</b-col>
+              </b-row>
+
+              <b-row>
+                <b-col>Output:</b-col>
+                <b-col>{{ row.item.output.size }}</b-col>
+              </b-row>
+            </b-col>
+            <b-col align-self="center" md="auto" class="m-2">
+              <b-button
+                v-if=" row.item.input.size || row.item.output.size"
+                size="sm"
+                variant="dark"
+                @click="row.toggleDetails"
+              >
+                <b-icon-arrow-bar-up v-if="row.detailsShowing" />
+                <b-icon-arrow-bar-down v-else />
+              </b-button>
+            </b-col>
           </b-row>
-        </b-card>
-      </template>
-    </b-table>
+        </template>
+
+        <template v-slot:row-details="row">
+          <b-card class="text-left" bg-variant="dark">
+            <b-row v-if="row.item.input.size">
+              <json-view
+                :maxDepth="1"
+                :rootKey="'Input'"
+                :data="row.item.input.data"
+                :colorScheme="'dark'"
+              />
+            </b-row>
+
+            <b-row v-if="row.item.output.size">
+              <json-view
+                :maxDepth="1"
+                :rootKey="'Output'"
+                :data="row.item.output.data"
+                :colorScheme="'dark'"
+              />
+            </b-row>
+          </b-card>
+        </template>
+      </b-table>
+    </b-container>
   </div>
 </template>
 
@@ -69,27 +97,58 @@ export default {
 
   components: { "json-view": JSONView },
 
+  created() {
+    this.fetch(this.currentPage, this.pageSize);
+  },
+
   methods: {
-    _size(obj) {
-      return Object.keys(obj).length;
+    _processStats(item) {
+      const tmp = item;
+      tmp.input = {
+        data: tmp.input,
+        size: this._objSize(tmp.input),
+      };
+      tmp.output = {
+        data: tmp.output,
+        size: this._objSize(tmp.output),
+      };
+      this.items.push(tmp);
+    },
+
+    changePage(newPage) {
+      this.currentPage = newPage;
+      this.fetch(this.currentPage, this.pageSize);
+    },
+
+    changePageSize(newPageSize) {
+      this.pageSize = newPageSize;
+      this.changePage(1);
+    },
+
+    _objSize(obj) {
+      return obj ? Object.keys(obj).length : 0;
+    },
+
+    fetch(page, size) {
+      this.items = [];
+      console.log(page, size);
+      this.$http({
+        url: "stats/news",
+        params: { page: page - 1, size: size },
+        method: "GET",
+      })
+        .then((response) => {
+          console.log(response.data);
+          response.data.stats.forEach(this._processStats);
+          this.pageCount = response.data.page_count;
+        })
+        .catch();
     },
   },
 
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          user: "",
-          action: "List News",
-          timestamp: "2013-02-08 24:00:00.000",
-          input: {
-            "wubba-laba-dub-dub": "pickle rick",
-            "asdfhklashfasf": {'aslkdnfhlsakhdflkasf': 'dsafphslfdnhkhas'},
-          },
-          output: {},
-        },
-      ],
+      items: [],
       fields: [
         {
           key: "id",
@@ -125,6 +184,8 @@ export default {
         { key: "io", label: "Input/Output", class: "align-middle" },
       ],
       currentPage: 1,
+      pageSize: 5,
+      pageCount: 1,
       pageOptions: [5, 10, 15, 20, 25],
     };
   },
@@ -132,4 +193,28 @@ export default {
 </script>
 
 <style>
+.pagination > li > a {
+  background-color: white;
+  color: #343a40;
+}
+
+.pagination > li > a:focus,
+.pagination > li > a:hover,
+.pagination > li > span:focus,
+.pagination > li > span:hover {
+  color: #343a40;
+  background-color: #eee;
+  border-color: #ddd;
+}
+
+.pagination > .active > a {
+  color: white;
+  background-color: #343a40 !important;
+  border: solid 1px #343a40 !important;
+}
+
+.pagination > .active > a:hover {
+  background-color: #343a40 !important;
+  border: solid 1px #343a40;
+}
 </style>
