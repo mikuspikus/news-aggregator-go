660ef0cef993b9e26f18189d5fc85fb5651b7a64
mikuspikus <pyro74rus@gmial.com>
2020-08-14 18:26:43 +0500

    Delete excess tabs

--- services/news/sql/script.sql
+++ services/news/sql/script.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE table news (
-                          uid uuid PRIMARY KEY,
-                          user_uid UUID not null,
-                          title text not null,
-                          uri text not null,
-                          created_at timestamp with time zone not null,
-                          edited_at timestamp with time zone not null
+    uid uuid PRIMARY KEY,
+    user_uid UUID not null,
+    title text not null,
+    uri text not null,
+    created_at timestamp with time zone not null,
+    edited_at timestamp with time zone not null
 );
 
 create view news_view as
