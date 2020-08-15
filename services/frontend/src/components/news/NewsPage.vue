538fdf0b82176d157e5c661ed1bfa981f535443d
mikuspikus <pyro74rus@gmial.com>
2020-08-15 15:21:41 +0500

    Add NewsPage and with fetching logic from News view

---
+++ services/frontend/src/components/news/NewsPage.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="news-page">
+    <template v-if="isLoading">
+      <b-button variant="white" disabled>
+        <b-spinner small type="grow" variant="dark" label="Loading" />Loading...
+      </b-button>
+    </template>
+
+    <template v-else-if="!ok">
+      <b-row class="text-left">
+        <b-col>
+          <service-error-card />
+        </b-col>
+      </b-row>
+    </template>
+
+    <template v-else>
+      <template v-if="!news.length || !pageCount">
+        <b-row class="text-left">
+          <b-col>
+            <service-error-card :header="'No news yet'" :lead="'Maybe you should add some?'" />
+          </b-col>
+        </b-row>
+      </template>
+      <template v-else>
+        <template v-for="snews in news">
+          <b-row :key="snews.uid">
+            <b-col>
+              <news-short :uid="snews.uid" :title="snews.title" :created="snews.created" />
+            </b-col>
+          </b-row>
+        </template>
+
+        <b-row class="mt-2">
+          <b-col>
+            <b-pagination-nav align="fill" :link-gen="linkGenerator" :number-of-pages="pageCount" />
+          </b-col>
+        </b-row>
+      </template>
+    </template>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ServiceErrorCard from "../utility/ServiceErrorCard.vue";
+import NewsShort from "../news/Short.vue";
+import errhandler from "../../utility/errhandler.js";
+
+export default {
+  name: "news-index",
+
+  components: { ServiceErrorCard, NewsShort },
+
+  data() {
+    return {
+      pageSize: 25,
+      isLoading: true,
+      ok: true,
+      news: [],
+      pageCount: 1,
+      page: this.$route.query.page ? this.$route.query.page : 0,
+    };
+  },
+
+  created() {
+    this.fetch(this.page);
+  },
+
+  methods: {
+    linkGenerator(pageNumber) {
+      return pageNumber === 1 ? "?" : `?page=${pageNumber}`;
+    },
+
+    fetch(page) {
+      this.$http({
+        url: "news/",
+        params: { page: page, size: this.pageSize },
+        method: "GET",
+      })
+        .then((response) => {
+          this.news = response.data.news;
+          this.pageCount = response.data.page_count;
+        })
+        .catch((error) => {
+          this.ok = false;
+
+          const { message, code } = errhandler.handle(error);
+          const title =
+            "News fetching error" + (code ? ` with code ${code}` : "");
+
+          this.$bvToast.toast(message, {
+            title: title,
+            autoHideDelay: 5000,
+            variant: "white",
+            toaster: "b-toaster-bottom-center",
+          });
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+  },
+};
+</script>
+
+<style>
+.pagination > li > a {
+  background-color: white;
+  color: #343a40;
+}
+
+.pagination > li > a:focus,
+.pagination > li > a:hover,
+.pagination > li > span:focus,
+.pagination > li > span:hover {
+  color: #343a40;
+  background-color: #eee;
+  border-color: #ddd;
+}
+
+.pagination > .active > a {
+  color: white;
+  background-color: #343a40 !important;
+  border: solid 1px #343a40 !important;
+}
+
+.pagination > .active > a:hover {
+  background-color: #343a40 !important;
+  border: solid 1px #343a40;
+}
+</style>
--- services/frontend/src/views/News.vue
+++ services/frontend/src/views/News.vue
@@ -4,53 +4,12 @@
       <b-row v-if="isLogged" class="mb-2 text-left">
         <b-col>
           <b-card>
-          <add-news-form />
+            <add-news-form />
           </b-card>
         </b-col>
       </b-row>
 
-      <template v-if="isLoading">
-        <b-button variant="white" disabled>
-          <b-spinner small type="grow" variant="dark" label="Loading" />Loading...
-        </b-button>
-      </template>
-
-      <template v-else-if="!ok">
-        <b-row class="text-left">
-          <b-col>
-            <service-error-card />
-          </b-col>
-        </b-row>
-      </template>
-
-      <template v-else>
-        <template v-if="!news.length || !pageCount">
-          <b-row class="text-left">
-            <b-col>
-              <service-error-card :header="'No news yet'" :lead="'Maybe you should add some?'" />
-            </b-col>
-          </b-row>
-        </template>
-        <template v-else>
-          <template v-for="snews in news">
-            <b-row :key="snews.uid">
-              <b-col>
-                <news-short :uid="snews.uid" :title="snews.title" :created="snews.created" />
-              </b-col>
-            </b-row>
-          </template>
-
-          <b-row class="mt-2">
-            <b-col>
-              <b-pagination-nav
-                align="fill"
-                :link-gen="linkGenerator"
-                :number-of-pages="pageCount"
-              />
-            </b-col>
-          </b-row>
-        </template>
-      </template>
+      <news-page />
     </b-container>
   </div>
 </template>
@@ -58,91 +17,17 @@
 <script>
 // @ is an alias to /src
 import AddNewsForm from "@/components/news/Add.vue";
-import ServiceErrorCard from "../components/utility/ServiceErrorCard.vue";
-import NewsShort from "../components/news/Short.vue";
+import NewsPage from "../components/news/NewsPage.vue";
 
 export default {
-  name: "news-index",
+  name: "index-view",
 
-  components: { ServiceErrorCard, NewsShort, AddNewsForm },
-
-  data() {
-    return {
-      pageSize: 25,
-      isLoading: true,
-      ok: true,
-      news: [],
-      pageCount: 1,
-      page: this.$route.query.page ? this.$route.query.page : 0,
-    };
-  },
+  components: { AddNewsForm, NewsPage },
 
   computed: {
     isLogged() {
       return this.$store.getters.isLogged;
     },
   },
-
-  created() {
-    this.fetch(this.page);
-  },
-
-  methods: {
-    linkGenerator(pageNumber) {
-      return pageNumber === 1 ? "?" : `?page=${pageNumber}`;
-    },
-
-    fetch(page) {
-      this.$http({
-        url: "news/",
-        params: { page: page, size: this.pageSize },
-        method: "GET",
-      })
-        .then((response) => {
-          this.news = response.data.news;
-          this.pageCount = response.data.page_count;
-        })
-        .catch((error) => {
-          this.ok = false;
-
-          this.$bvToast.toast(error, {
-            title: "Error",
-            autoHideDelay: 5000,
-            variant: "white",
-            toaster: "b-toaster-bottom-center",
-          });
-        })
-        .finally(() => {
-          this.isLoading = false;
-        });
-    },
-  },
 };
 </script>
-
-<style>
-.pagination > li > a {
-  background-color: white;
-  color: #343a40;
-}
-
-.pagination > li > a:focus,
-.pagination > li > a:hover,
-.pagination > li > span:focus,
-.pagination > li > span:hover {
-  color: #343a40;
-  background-color: #eee;
-  border-color: #ddd;
-}
-
-.pagination > .active > a {
-  color: white;
-  background-color: #343a40 !important;
-  border: solid 1px #343a40 !important;
-}
-
-.pagination > .active > a:hover {
-  background-color: #343a40 !important;
-  border: solid 1px #343a40;
-}
-</style>
